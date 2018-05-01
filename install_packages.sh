d77f501d88cb82f3de04933706ccb9893be24984
Matthew Long <thematray@gmail.com>
2018-05-01 17:36:16 -0400

    Fixed installation scripts so they work

--- install_packages.sh
+++ install_packages.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get upgrade -y
 cat packages.list | xargs sudo apt-get install -y
-sudo apt-get install linux-headers-$(uname -r)
+sudo apt-get install linux-headers-$(uname -r) -y
+sudo apt-get autoremove -y
--- install_python_packages.sh
+++ install_python_packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo pip install --upgrade pip
-sudo pip3 install --upgrade pip3
+sudo pip3 install --upgrade pip
 sudo pip install --upgrade virtualenv
 sudo pip3 install --upgrade virtualenv
 sudo pip install -r requirements.txt
